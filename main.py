3a0509e31c17695d2b5a65bcd160e572c5121ce3
root <dr10n9@gmail.com>
2018-05-01 12:15:10 +0000

    pep8 for main.py

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ import requests
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
     return(open("/app/index.html", "r").read())
 
+
 @app.route('/ip')
 def ip():
     r = requests.get(r'http://jsonip.com')
-    ip= r.json()['ip']
+    ip = r.json()['ip']
     return(ip)
+
 
 @app.route('/css/style.css')
 def style():
     return(open('/app/css/style.css', 'r').read())
 
+
 @app.route('/images')
 def image():
-    return send_file('/app/images/'+request.args.get('name'), mimetype='image/gif')
-###def application(env, start_response):###
-   ### start_response('200 OK', [('Content-Type', 'text/html')])###
-   ### return [b"Hello from MacPaw"]###
+    return send_file('/app/images/'+request.args.get('name'),
+                     mimetype='image/gif')
